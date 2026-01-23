a6b15437f84402b8d69ec4705fe3f2747b508c1f
HR-Shekhar <mehimanshusss@gmail.com>
2026-01-23 20:02:48 +0530

    implemented and tested grep both case sensitive and insensitive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use std::clone;
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+use std::process::Command;
+use minigrep::{search_case_insensitive, search};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
+    }
 }
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+         println!("{line}");
+    }
+
+    Ok(())  //using () like this is the idiomatic way to indicate that we’re calling run for its side effects only; it doesn’t return a value we need.
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+    ignore_case: bool,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        
+        Ok(Config {query, file_path, ignore_case})
+    }
+}
+
